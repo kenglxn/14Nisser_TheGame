966936597a426281969a499088f453d657552543
Ken Gullaksen <ken.gullaksen@embriq.no>
2014-12-22 16:22:02 +0100

    prepare for game based on the platformer example

--- platformer.js
+++ platformer.js
@@ -438,15 +438,4 @@ Q.loadTMX("level1.tmx, collectables.json, doors.json, enemies.json, fire.mp3, ju
   }
 });
 
-// ## Possible Experimentations:
-// 
-// The are lots of things to try out here.
-// 
-// 1. Modify level.json to change the level around and add in some more enemies.
-// 2. Add in a second level by creating a level2.json and a level2 scene that gets
-//    loaded after level 1 is complete.
-// 3. Add in a title screen
-// 4. Add in a hud and points for jumping on enemies.
-// 5. Add in a `Repeater` behind the TileLayer to create a paralax scrolling effect.
-
 });
